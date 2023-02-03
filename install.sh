d6a300b56fc2c085509dfa74e95aa4c716e27660
CyberSecHero <wanguivalentine19@gmail.com>
2023-02-03 23:37:23 +0300

    --updates -bash commands

--- install.sh
+++ install.sh
@@ -4,6 +4,13 @@ cd /opt/lampp/htdocs/
 mkdir HACKLAB && cd HACKLAB && mkdir LTQS_SQLINJECTION
 cp -R * /opt/lampp/htdocs/HACKLAB/LTQS_SQLINJECTION/
 
+#starting server
+echo "[*] start xampp server"
+echo "[!] start apache server && mysqli"
+time(2)
+sudo /opt/lampp/manager-linux-x64.run
+echo "Link generated"
+echo "http://localhost/HACKLAB/LTQS_SQLINJECTION/"
 else
 echo "DOWNLOAD XAMPP"
 fi;
